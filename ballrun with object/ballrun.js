4201b2f72a2f83310959f7c68161e518fc53afc7
mojahmed <mojahmed93@gmail.com>
2022-04-11 14:00:09 -0400

    ball1 working

--- ballrun with object/ballrun.js
+++ ballrun with object/ballrun.js
@@ -8,6 +8,12 @@ function constructBall() {
   //      color
   //      velocity ({x: 10, y: 0})
   //      domElement (document.querySelector("#ball"))
+ballObject.radius = 20;
+ballObject.position ={x: 250, y: 250};
+ballObject.color = "red"
+ballObject.velocity = {x: 10, y: 0};
+ballObject.domElement =  document.querySelector(".ball");
+
 
   // Add the following starting styles to the domElement:
   //  - width = radius + "px"
@@ -15,6 +21,14 @@ function constructBall() {
   //  - backgroundColor = color
   //  - top = position.y + "px"
   //  - left = position.x + "px"
+    console.log(ballObject);
+    ballObject.domElement.style.width = ballObject.radius + "px";
+    ballObject.domElement.style.height = ballObject.radius + "px";
+    ballObject.domElement.style.backgroundColor = ballObject.color;
+    ballObject.domElement.style.top = ballObject.position.y +"px";
+    ballObject.domElement.style.left = ballObject.position.x +"px";
+
+
 }
 
 function move() {
@@ -27,10 +41,14 @@ function setPosition() {
   //   - update the properties of the ballObject
   //   - run the drawPosition() function
   // The position of the ball should be the current position plus the velocity.
+  ballObject.position.x = ballObject.position.x + ballObject.velocity.x;
+  ballObject.position.y = ballObject.position.y + ballObject.velocity.y;
 }
 
 function drawPosition() {
   // When this function runs, we need to:
   //   - use the properties of the ballObject to update the style of the ball domElement
   // Remember that CSS requires a string with "px" at the end. You can use x + "px"
+  ballObject.domElement.style.top = ballObject.position.y + "px";
+  ballObject.domElement.style.left = ballObject.position.x + "px";
 }
--- ballrun/ballrun.js
+++ ballrun/ballrun.js
@@ -1,7 +1,11 @@
 var ball = document.querySelector("#ball");
 var left = 0;
+var top = 
 ball.style.left = left + "px";
 
 function move() {
+  left = left + 20;
   ball.style.left = left + "px";
 }
+
+ball.onclick(move)
